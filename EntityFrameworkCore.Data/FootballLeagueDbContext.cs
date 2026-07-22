d1f4eb7cf3eaa379dd647c810ec211224bd0ebb9
ali3059 <ali3059@DESKTOP-NNMSVH6>
2026-07-22 00:42:20 -0700

    Revert "sqlLiteconnection"
    This reverts commit 39e97060c728730380044544edb472fef8876c94.

--- EntityFrameworkCore.Data/FootballLeagueDbContext.cs
+++ EntityFrameworkCore.Data/FootballLeagueDbContext.cs
@@ -1,6 +1,5 @@
 ﻿using EntityFrameworkCore.Domain;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Identity.Client;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,14 +8,7 @@ using System.Text;
 namespace EntityFrameworkCore.Data
 {
     internal class FootballLeagueDbContext : DbContext
-
     {
-        //public FootballLeagueDbContext()
-        //{
-        //    var folder = Environment.SpecialFolder.LocalApplicationData;
-        //    var path = Environment.GetFolderPath(folder);
-        //    DbPath = Path.Combine
-        //}
         public DbSet<Team> Teams { get; set; }
         public DbSet<Coach> Coaches { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -24,34 +16,7 @@ namespace EntityFrameworkCore.Data
             //using sql server
             //optionsBuilder.UseSqlServer("Data Source=(localdb)\\ProjectModels;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30");
 
-            optionsBuilder.UseSqlite($"Data Source = FootballLeage_EfCore.db")
-                //.LogTo(Console.WriteLine, LogLevel.Information)
-                .EnableSensitiveDataLogging()
-                .EnableDetailedErrors();
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Team>().HasData(
-                new Team
-                {
-                    TeamId = 1,
-                    TeamName = "eagles",
-                    CreatedDate = DateTimeOffset.UtcNow.DateTime
-                }
-                new Team
-                {
-                    TeamId = 2,
-                    TeamName = "Argentina",
-                    CreatedDate = DateTimeOffset.UtcNow.DateTime
-                }
-                new Team
-                {
-                    TeamId = 3,
-                    TeamName = "Spain",
-                    CreatedDate = DateTimeOffset.UtcNow.DateTime
-                }
-                );
+            optionsBuilder.UseSqlite($"Data Source = FootballLeage_EfCore_db");
         }
     }
 }
