39e97060c728730380044544edb472fef8876c94
ali3059 <ali3059@DESKTOP-NNMSVH6>
2026-07-22 00:41:50 -0700

    sqlLiteconnection

---
+++ EntityFramworkCore.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using EntityFrameworkCore.Data;
+
+
+using EntityFrameworkCore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+using var context = new FootballLeagueDbContext();
+
+//selecting a single record
+
+var teamOne = await context.Teams.FirstAsync();
+
+var teamTwo = await context.Teams.FirtAsync(taem => team.TeamId == 1);
+
+var teamThree = await context.Teams.FirstAsync(teamOne => teamOne.TeamId == 2);
+
+void GetAllTeams()
+{
+    var teams = context.Teams.ToList();
+
+    foreach (var t in teams)
+    {
+        Console.WriteLine(team.Name);
+    }
+}
